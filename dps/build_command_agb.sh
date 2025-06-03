4488906571b65fe117d9fd3bc2989bd163649b32
aliz237 <ali.zaringhalam@gmail.com>
2025-06-03 13:07:57 -0700

    updated build command

--- dps/build_command_agb.sh
+++ dps/build_command_agb.sh
@@ -7,14 +7,5 @@ conda env create -f ${basedir}/above_env_r_3.1.4.yml
 
 pushd ${HOME}
 
-# Do not remove this (PMM Dec 2022)
-source activate icesat2_boreal
-
-# needed for ee asset export
-#mamba install --name icesat2_boreal -c conda-forge earthengine-api
-
-# pip install git+https://github.com/MAAP-Project/maap-py.git@v4.1.0
-
 source activate r
-conda install -c conda-forge r-optparse r-ranger -y
-#pip3 install pyOpenSSL --upgrade
+conda install -c conda-forge r-optparse r-ranger r-ggplot2 -y
